edf6fedfa0dd92e1bdea4488102622201782578b
LuluWalls <36511036+LuluWalls@users.noreply.github.com>
2018-03-19 11:58:13 -0400

    Add files via upload

--- src/imp_ene_solids/Lulu_Beachballs_core.rb
+++ src/imp_ene_solids/Lulu_Beachballs_core.rb
@@ -3,32 +3,39 @@
 # https://github.com/LuluWalls/Beachballs
 #
 # Example:
-#   module Test_Lulu
-#     def self.spin()
-#       # start the spinning beach ball
-#       Lulu::Beachballs.start()
-#     
-#       #perform a long calculation
-#       a = 0
-#       40000000.times do
-#         a = a + 1
-#       end
-#     
-#       # Throw an exception
-#       #x = 10/0
-#   
-#       # always ensure you didn't drop the ball
-#       ensure
-#       Lulu::Beachballs.stop()
-#       
-#     end # sin  
-#     
-#     toolbar = UI::Toolbar.new "Test Beachballs"
-#     cmd = UI::Command.new("Test") {spin()}
-#     toolbar = toolbar.add_item cmd
-#     toolbar.show
-#   
-#   end #module
+# module Test_Lulu
+#  def self.spin()
+#    # start the ball spinning
+#    Lulu::Beachballs.start()
+#    Sketchup.active_model.start_operation('balls', true)
+#    
+#    # perform a long calculation
+#    1000.times do
+#      make_block()
+#    end
+#  
+#    # always ensure you didn't drop the ball
+#    ensure
+#    Sketchup.active_model.commit_operation
+#    Lulu::Beachballs.stop()
+#  end # spin  
+#
+#  def self.make_block()
+#    ents = Sketchup.active_model.entities
+#    grp = ents.add_group
+#
+#    face = grp.entities.add_face [0,0,0],[1,0,0],[1,1,0],[0,1,0]
+#    face.pushpull(1)
+#    
+#    tr = Geom::Transformation.new [rand(100),rand(100),rand(100)]
+#    grp.transform!(tr)
+#  end
+#  
+#  toolbar = UI::Toolbar.new "Test Beachballs"
+#  cmd = UI::Command.new("Test") {spin()}
+#  toolbar = toolbar.add_item cmd
+#  toolbar.show
+# end
 #
 # Methods 
 #   start()
